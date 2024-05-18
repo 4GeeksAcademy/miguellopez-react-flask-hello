ddfe5cff4ea1bd567137ad793213280bbcaca6cf
Miguel López <155157447+Miguelangel-lf@users.noreply.github.com>
2024-05-18 09:30:51 +0000

    miguellrfa

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,18 +5,40 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, JWTManager,get_jwt_identity, jwt_required
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+@api.route("/crear_usuario", methods =["POST"])
+def crear_usuario():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(email=email,password=password).first()
+    if user:
+        return jsonify ({"msg":"Este usuario ya existe"}), 401
+    user_new = User(email=email,password=password)
+    db.session.add(user_new)
+    db.session.commit()
+    return jsonify({"msg":"Usuario creado"}), 200
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route("/iniciar_sesion", methods =["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(email=email,password=password).first()
+    if user is None:
+        return jsonify({"msg":"Usuario no encontrado"}), 401
+    access_token = create_access_token(identity = user.id)
+    return jsonify({"token":access_token,"user.id":user.id}), 200
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
+@api.route("/protected" , methods = ["GET"])
+@jwt_required()
+def protected():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    if user is None:
+        return APIException("Usuario no encontrado", status_code = 404)
+    return jsonify("Usuario autenticado"),200
---
+++ src/front/js/component/registroUsuario.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+const Signup = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('/signup', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ email, password }),
+            });
+            if (response.ok) {
+                window.location.href = '/private';
+            } else {
+                // Handle login error
+            }
+        } catch (error) {
+            console.error('Error during login:', error);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+    );
+}
+
+export default Signup;
